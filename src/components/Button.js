c861f6addacb30aff5bf753bb427ad8c3d458ea0
Margot <margotdupont@hotmail.fr>
2020-04-20 18:22:29 +0200

    add effects on the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,14 @@ import React from 'react'
 import './Button.css'
 
 const Button = ({isClicked, text}) => {
-    return (
-    <button onClick={isClicked} className='button'>{text}
+  return (
+    <button onClick={isClicked} className='button'>
+        <svg width="370px" height="60px" viewBox="0 0 370 60" class="border">
+          <polyline points="369,1 369,59 1,59 1,1 369,1" class="bg-line" />
+          <polyline points="369,1 369,59 1,59 1,1 369,1" class="hl-line" />
+        </svg>
+    <span>{text}</span>
+    
     </button>
     )
 }
