2a08144b746fe0ef228a72dda1da1e5ef38f8a07
Lenasim <radical.rin@gmail.com>
2020-04-21 16:38:30 +0200

    Task 9 finished : CSS modal (Responsive) with hard coded datas

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import Modal from './Modal'
 
-
-class Card extends React.Component {
-    state = {
-        modal: false
-    }
-
-    toggleModal = () => {
-        this.setState({modal: !this.state.modal})
-    }
-
-    render() {
-        return (
-            <div>
-            <a className="card-link" href="\#" onClick={this.toggleModal} >
-                <div className="card">
-                    <div className="card-details">
-                        <h4 className="card-category">{this.props.categorie}</h4>
-                        <h3 className="card-title">{this.props.name}</h3>
-                    </div>
-                    <img className="card-image" src={this.props.image} alt={this.props.name} />
+function Card(props) {
+    return (
+        <div className="card-link" onClick={props.onClick} >
+            <div className="card">
+                <div className="card-details">
+                    <h4 className="card-category">{props.categorie}</h4>
+                    <h3 className="card-title">{props.name}</h3>
                 </div>
-            </a>
-            <Modal show={this.state.modal} handleClose={this.toggleModal}/>
+                <img className="card-image" src={props.image} alt={props.name} />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Card
