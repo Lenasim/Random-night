673d9c8f06627be6ae6c9a90b76c81f16f36fe58
Lola <lola.donval@gmail.com>
2020-04-21 16:11:45 +0200

    add filter button and onClick method to App.js

--- src/App.js
+++ src/App.js
@@ -9,35 +9,45 @@ import './App.css';
 class App extends Component {
   state = {
     firstClick: false,
-    textButton: 'Laisse toi faire !'
+    textButton: 'Laisse toi faire !',
+    filterClick: false,
+    textFilterButton: 'Personnaliser'
   }
 
   handleFirstClick = () => {
-      this.setState({firstClick: true})
+    this.setState({ firstClick: true })
   }
 
   handleReset = () => {
-      this.setState({firstClick: false})
-    }
+    this.setState({ firstClick: false })
+  }
+
+  handleFilterClick = () => {
+    this.setState({ filterClick: true })
+  }
 
   render() {
     return (
       <div className="App">
-        <Header reset={this.handleReset}/>
+        <Header reset={this.handleReset} />
         <h1 className="punchline">
           {
             this.state.firstClick ?
-            'tadaaam !'
+              'tadaaam !'
               : 'Tu sais pas quoi faire ce soir ?'
           }
         </h1>
-        {this.state.firstClick ? 
+        {this.state.firstClick ?
           <Fragment />
-          : <Button 
+          : <Button
             isClicked={this.handleFirstClick}
             text={this.state.textButton}
           />
         }
+        <Button
+          isClicked={this.handleFilterClick}
+          text={this.state.textFilterButton}
+        />
         {this.state.firstClick ? <CardsList /> : <Fragment />}
       </div>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import './Button.css'
 
-const Button = ({isClicked, text}) => {
+const Button = ({ isClicked, text, loader }) => {
   return (
-    <button onClick={isClicked} className='button'>
-        <svg width="370px" height="60px" viewBox="0 0 370 60" className="border">
-          <polyline points="369,1 369,59 1,59 1,1 369,1" className="bg-line" />
-          <polyline points="369,1 369,59 1,59 1,1 369,1" className="hl-line" />
-        </svg>
-    <span>      
+    <button
+      onClick={isClicked}
+      className='button'>
       {loader && <i className="fa fa-pulse fa-spinner"></i>}
       {!loader && text}
-    </span>
     </button>
   )
 }
