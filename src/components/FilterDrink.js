99274f9f31100b28be4bdcfba6ad56483bfa66fb
Lola <lola.donval@gmail.com>
2020-04-22 11:49:31 +0200

    fix button position

--- src/components/FilterDrink.js
+++ src/components/FilterDrink.js
@@ -4,31 +4,29 @@ import './FilterDrink.css'
 class FilterDrink extends Component {
   render() {
     return (
-      <div id="drink" className="FilterDrink">
-        <div className="form-group">
-          <select name="categories" id="drink-categories">
-            <option className="option" value="Catégories">Catégories</option>
-            <option className="option" value="Cocktail">Cocktail</option>
-            <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
-            <option className="option" value="Ordinary Drink">Ordinary Drink</option>
-            <option className="option" value="Cocoa">Cocoa</option>
-          </select>
-          <select name="ingrédients" id="drink-ingrédients">
-            <option className="option" value="Ingrédients">Ingrédients</option>
-            <option className="option" value="Cocktail">Cocktail</option>
-            <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
-            <option className="option" value="Ordinary Drink">Ordinary Drink</option>
-            <option className="option" value="Cocoa">Cocoa</option>
-          </select>
-          <div className="checkbox">
-            <div className="checkbox-group">
-              <input className="checkbox-box" type="checkbox" id="alcool" />
-              <label className="checkbox-item" htmlFor="alcool">Avec alcool</label>
-            </div>
-            <div className="checkbox-group">
-              <input className="checkbox-box" type="checkbox" id="soft" />
-              <label className="checkbox-item" htmlFor="soft">Sans alcool</label>
-            </div>
+      <div className="FilterDrink">
+        <select name="categories" id="drink-categories">
+          <option className="option" value="Catégories">Catégories</option>
+          <option className="option" value="Cocktail">Cocktail</option>
+          <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
+          <option className="option" value="Ordinary Drink">Ordinary Drink</option>
+          <option className="option" value="Cocoa">Cocoa</option>
+        </select>
+        <select name="ingrédients" id="drink-ingrédients">
+          <option className="option" value="Ingrédients">Ingrédients</option>
+          <option className="option" value="Cocktail">Cocktail</option>
+          <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
+          <option className="option" value="Ordinary Drink">Ordinary Drink</option>
+          <option className="option" value="Cocoa">Cocoa</option>
+        </select>
+        <div className="checkbox">
+          <div className="checkbox-group">
+            <input className="checkbox-box" type="checkbox" id="alcool" />
+            <label className="checkbox-item" htmlFor="alcool">Avec alcool</label>
+          </div>
+          <div className="checkbox-group">
+            <input className="checkbox-box" type="checkbox" id="soft" />
+            <label className="checkbox-item" htmlFor="soft">Sans alcool</label>
           </div>
         </div>
         <button className="form-btn">Chercher</button>
