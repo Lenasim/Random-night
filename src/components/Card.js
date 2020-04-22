a79d8996b22b10040f888774657447ede9f98239
Lola <lola.donval@gmail.com>
2020-04-22 10:56:40 +0200

    fix css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
 function Card(props) {
-    return (
-        <div className="card-link" onClick={props.onClick} >
-            <div className="card">
-                <div className="card-details">
-                    <h4 className="card-category">{props.categorie}</h4>
-                    <h3 className="card-title">{props.name}</h3>
-                </div>
-                <img className="card-image" src={props.image} alt={props.name} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="card" onClick={props.onClick} >
+      <div className="card-details">
+        <h4 className="card-category">{props.categorie}</h4>
+        <h3 className="card-title">{props.name}</h3>
+      </div>
+      <img className="card-image" src={props.image} alt={props.name} />
+    </div>
+  )
 }
 
 export default Card
