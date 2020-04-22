602a21a393dd197a1d4be12808cf0e1a88e80446
Lola <lola.donval@gmail.com>
2020-04-22 12:06:09 +0200

    put some fake data to selcet-form

--- src/components/FilterMovie.js
+++ src/components/FilterMovie.js
@@ -5,15 +5,15 @@ class FilterMovie extends Component {
   render() {
     return (
       <div id="movie" className="FilterMovie">
-        <select name="categories" id="drink-categories">
-          <option className="option" value="Catégories">Catégories</option>
+        <select name="genres" id="movie-genres">
+          <option className="option" value="genre">Genres</option>
           <option className="option" value="Cocktail">Cocktail</option>
           <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
           <option className="option" value="Ordinary Drink">Ordinary Drink</option>
           <option className="option" value="Cocoa">Cocoa</option>
         </select>
-        <select name="ingrédients" id="drink-ingrédients">
-          <option className="option" value="Ingrédients">Ingrédients</option>
+        <select name="languages" id="movie-languages">
+          <option className="option" value="language">Langue</option>
           <option className="option" value="Cocktail">Cocktail</option>
           <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
           <option className="option" value="Ordinary Drink">Ordinary Drink</option>
@@ -21,12 +21,12 @@ class FilterMovie extends Component {
         </select>
         <div className="checkbox">
           <div className="checkbox-group">
-            <input className="checkbox-box" type="checkbox" id="alcool" />
-            <label className="checkbox-item" htmlFor="alcool">Avec alcool</label>
+            <input className="checkbox-box" type="checkbox" id="all" />
+            <label className="checkbox-item" htmlFor="all">Tous publics</label>
           </div>
           <div className="checkbox-group">
-            <input className="checkbox-box" type="checkbox" id="soft" />
-            <label className="checkbox-item" htmlFor="soft">Sans alcool</label>
+            <input className="checkbox-box" type="checkbox" id="adult" />
+            <label className="checkbox-item" htmlFor="adult">Pour les grands</label>
           </div>
         </div>
         <button className="form-btn">Chercher</button>
--- src/components/FilterRecipe.js
+++ src/components/FilterRecipe.js
@@ -5,30 +5,27 @@ class FilterRecipe extends Component {
   render() {
     return (
       <div id="recipe" className="FilterRecipe">
-        <select name="categories" id="drink-categories">
-          <option className="option" value="Catégories">Catégories</option>
+        <select name="categories" id="recipe-categories">
+          <option className="option" value="categorie">Catégories</option>
           <option className="option" value="Cocktail">Cocktail</option>
           <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
-          <option className="option" value="Ordinary Drink">Ordinary Drink</option>
+          <option className="option" value="Ordinary recipe">Ordinary recipe</option>
           <option className="option" value="Cocoa">Cocoa</option>
         </select>
-        <select name="ingrédients" id="drink-ingrédients">
+        <select name="ingredients" id="recipe-ingredients">
           <option className="option" value="Ingrédients">Ingrédients</option>
           <option className="option" value="Cocktail">Cocktail</option>
           <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
-          <option className="option" value="Ordinary Drink">Ordinary Drink</option>
+          <option className="option" value="Ordinary recipe">Ordinary recipe</option>
           <option className="option" value="Cocoa">Cocoa</option>
         </select>
-        <div className="checkbox">
-          <div className="checkbox-group">
-            <input className="checkbox-box" type="checkbox" id="alcool" />
-            <label className="checkbox-item" htmlFor="alcool">Avec alcool</label>
-          </div>
-          <div className="checkbox-group">
-            <input className="checkbox-box" type="checkbox" id="soft" />
-            <label className="checkbox-item" htmlFor="soft">Sans alcool</label>
-          </div>
-        </div>
+        <select name="countries" id="recipe-countries">
+          <option className="option" value="country">Pays</option>
+          <option className="option" value="Cocktail">Cocktail</option>
+          <option className="option" value="Milk / Float / Shake">Milk / Float / Shake</option>
+          <option className="option" value="Ordinary recipe">Ordinary recipe</option>
+          <option className="option" value="Cocoa">Cocoa</option>
+        </select>
         <button className="form-btn">Chercher</button>
       </div>
     );
