ad69c1a1d0fa71115d49d92474bb5020f71d8056
Lola <lola.donval@gmail.com>
2020-04-24 12:09:31 +0200

    change Button css

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,14 +4,9 @@ import './Button.css'
 const Button = ({ isClicked, text, loader }) => {
   return (
     <button onClick={isClicked} className='button'>
-      <svg width="370px" height="60px" viewBox="0 0 370 60" className="border">
-        <polyline points="369,1 369,59 1,59 1,1 369,1" className="bg-line" />
-        <polyline points="369,1 369,59 1,59 1,1 369,1" className="hl-line" />
-      </svg>
-      <span>
-        {loader && <i className="fa fa-pulse fa-spinner"></i>}
-        {!loader && text}
-      </span>
+      {loader && <i className="fa fa-pulse fa-spinner"></i>}
+      {!loader && text}
+      <span></span><span></span><span></span><span></span>
     </button>
   )
 }
