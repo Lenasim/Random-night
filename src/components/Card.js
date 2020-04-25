acab57a8fdee4c38f6eace78000a66e20caa7fc9
Lola <lola.donval@gmail.com>
2020-04-25 17:16:27 +0200

    add star icon to Card.js and css to Card.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 function Card(props) {
-  return (
-    <div className="card" onClick={props.onClick} >
-      <div className="card-details">
-        <h4 className="card-category">{props.categorie}</h4>
-        <h3 className="card-title">{props.name}</h3>
-      </div>
-      <img className="card-image" src={props.image} alt={props.name} />
-    </div>
-  )
+    return (
+        <div>
+            <div className="card"  >
+                <div className="card-details">
+                    <h4 className="card-category">{props.categorie}</h4>
+                    <h3 className="card-title"><i onClick={props.isFav} className={props.class}></i> {props.name}</h3>
+                </div>
+                <img className="card-image" src={props.image} alt={props.name} />
+            </div>
+        </div>
+
+    )
 }
 
 export default Card
