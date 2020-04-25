5b807b1b0e18ac6598b7813d096423eefb17fc67
Lola <lola.donval@gmail.com>
2020-04-25 17:22:39 +0200

    add start and fix function sur total random result

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -16,31 +16,47 @@ class CardsList extends Component {
     drinks: '',
     meals: '',
     movies: '',
+    ingDrink: [],
+    ingMeal: [],
     categories: [
-      'cocktail',
+      'drink',
       'movie',
       'recipe'
     ],
-    modal: false
+    isFavDrink: false,
+    favDrink: '',
+    isFavRecipe: false,
+    favRecipe: '',
+    isFavMovie: false,
+    favMovie: ''
   }
 
-  getRandom = () => {
+  getRandomDrink = () => {
+    this.setState({ loading: true })
+    axios
+      .get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+      .then(res => this.setState({ drinks: res.data.drinks[0] }, () => {
+        this.setState({ loaded: true, loading: false })
+      }))
+  }
+
+  getRandomRecipe = () => {
+    this.setState({ loading: true })
+    axios
+      .get('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then(resR => this.setState({ meals: resR.data.meals[0] }, () => {
+        this.setState({ loaded: true, loading: false })
+      }))
+  }
+
+  getRandomMovie = () => {
     this.setState({ loading: true })
     let pageMovie = Math.floor(Math.random() * 501)
     let resultMovie = Math.floor(Math.random() * 19)
-    axios.all([
-      axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php'),
-      axios.get('https://www.themealdb.com/api/json/v1/1/random.php'),
-      axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=439ba5790e4522ad15e0c6a3574cd795&language=en-US&page=${pageMovie}`)
-    ])
-      .then(axios.spread((resDrink, resMeal, resMovie) => {
-        this.setState({
-          drinks: resDrink.data.drinks[0],
-          meals: resMeal.data.meals[0],
-          movies: resMovie.data.results[resultMovie]
-        }, () => {
-          this.setState({ loaded: true, loading: false })
-        })
+    axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=439ba5790e4522ad15e0c6a3574cd795&language=en-US&page=${pageMovie}`)
+      .then(resM => this.setState({ movies: resM.data.results[resultMovie] }, () => {
+        this.setState({ loaded: true, loading: false })
       }))
   }
 
@@ -48,8 +64,43 @@ class CardsList extends Component {
     this.getRandom()
   }
 
-  toggleModal = () => {
-    this.setState({ modal: !this.state.modal })
+  isFavDrink = () => {
+    this.setState({ isFavDrink: !this.state.isFavDrink })
+    this.setState({ favDrink: this.state.drinks })
+  }
+
+  isFavRecipe = () => {
+    this.setState({ isFavRecipe: !this.state.isFavRecipe })
+    this.setState({ favRecipe: this.state.meals })
+  }
+
+  isFavMovie = () => {
+    this.setState({ isFavMovie: !this.state.isFavMovie })
+    this.setState({ favMovie: this.state.movies })
+  }
+
+  getRandom = () => {
+    const { isFavDrink, isFavMovie, isFavRecipe } = this.state
+    if (isFavDrink === false && isFavRecipe === false && isFavMovie === false) {
+      this.getRandomDrink()
+      this.getRandomRecipe()
+      this.getRandomMovie()
+    } else if (isFavDrink === true && isFavRecipe === true && isFavMovie === false) {
+      this.getRandomMovie()
+    } else if (isFavDrink === true && isFavRecipe === false && isFavMovie === true) {
+      this.getRandomRecipe()
+    } else if (isFavDrink === false && isFavRecipe === true && isFavMovie === true) {
+      this.getRandomDrink()
+    } else if (isFavDrink === true && isFavRecipe === false && isFavMovie === false) {
+      this.getRandomRecipe()
+      this.getRandomMovie()
+    } else if (isFavDrink === false && isFavRecipe === false && isFavMovie === true) {
+      this.getRandomDrink()
+      this.getRandomRecipe()
+    } else if (isFavDrink === false && isFavRecipe === true && isFavMovie === false) {
+      this.getRandomDrink()
+      this.getRandomMovie()
+    }
   }
 
   render() {
@@ -67,17 +118,26 @@ class CardsList extends Component {
               image={drinks.strDrinkThumb}
               name={drinks.strDrink}
               categorie={categories[0]}
-              onClick={this.toggleModal}  />
+              onClick={this.toggleModalDrink}
+              isFav={this.isFavDrink}
+              class={this.state.isFavDrink ? "fas fa-star" : "far fa-star"}
+            />
             <Card
               image={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`}
               name={movies.title}
               categorie={categories[1]}
-              onClick={this.toggleModal} />
+              onClick={this.toggleModalMovie}
+              isFav={this.isFavMovie}
+              class={this.state.isFavMovie ? "fas fa-star" : "far fa-star"}
+            />
             <Card
               image={meals.strMealThumb}
               name={meals.strMeal}
               categorie={categories[2]}
-              onClick={this.toggleModal} />
+              onClick={this.toggleModalRecipe}
+              isFav={this.isFavRecipe}
+              class={this.state.isFavRecipe ? "fas fa-star" : "far fa-star"}
+            />
           </div>
         }
         <Modal
