499035237e67ee32df582c8e271676acd86da1df
Lola <lola.donval@gmail.com>
2020-04-25 17:10:26 +0200

    add button + hover effect to open modal and remove hover effect to the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 function Card(props) {
-  return (
-    <div className="card" onClick={props.onClick} >
-      <div className="card-details">
-        <h4 className="card-category">{props.categorie}</h4>
-        <h3 className="card-title">{props.name}</h3>
-      </div>
-      <img className="card-image" src={props.image} alt={props.name} />
-    </div>
-  )
+    return (
+        <div>
+            <div className="card"  >
+                <div className="card-details">
+                    <h4 className="card-category">{props.categorie}</h4>
+                    <h3 className="card-title"> {props.name}</h3>
+                </div>
+                <img className="card-image" src={props.image} alt={props.name} />
+                <div onClick={props.onClick} className="more">
+                    <span className="plus">+</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Card
