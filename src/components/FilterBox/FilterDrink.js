1fb6ed593ebb9ee5037b1ff3024469b9b4bb0883
Lola <lola.donval@gmail.com>
2020-05-11 11:31:25 +0200

    fix css card-details and reorganize code

--- src/components/FilterBox/FilterDrink.js
+++ src/components/FilterBox/FilterDrink.js
@@ -10,7 +10,9 @@ class FilterDrink extends Component {
 
   getCategoriesDrink = () => {
     axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
-      .then(res => this.setState({ categories: res.data.drinks.map(c => c.strCategory) }))
+      .then(res => this.setState({
+        categories: res.data.drinks.map(c => c.strCategory)
+      }))
   }
 
   componentDidMount = () => {
@@ -18,7 +20,7 @@ class FilterDrink extends Component {
   }
 
   filterAlcohol = (e) => {
-    this.setState ({ isAlcohol: e.target.value })
+    this.setState({ isAlcohol: e.target.value })
     const isAlcohol = e.target.value
     this.props.handleAlcoholChange(isAlcohol)
   }
@@ -30,18 +32,72 @@ class FilterDrink extends Component {
   render() {
     return (
       <div className="FilterDrink">
-        <select name="selectedCategory" id="drink-categories" value={this.props.cat} onChange={this.filterCategory}>
-          <option className="option" value="categories">All categories</option>
-          {this.state.categories.map(cat => (<option className="option" value={cat} key={cat}>{cat}</option>))}
+        <select
+          name="selectedCategory"
+          id="drink-categories"
+          value={this.props.cat}
+          onChange={this.filterCategory}
+        >
+          <option
+            className="option"
+            value="categories"
+          >
+            All categories
+            </option>
+          {
+            this.state.categories.map(cat => (
+              <option
+                className="option"
+                value={cat}
+                key={cat}
+              >
+                {cat}
+              </option>
+            ))}
         </select>
-
         <div className="switch-field">
-          <input type="radio" name="nonAlcohol" value="all" id="all" onChange={this.filterAlcohol} checked={this.props.alc === 'all'} />
-          <label htmlFor="all" className="radio-label">All types</label>
-          <input type="radio" name="nonAlcohol" value="alcohol" id="alcohol"  onChange={this.filterAlcohol} checked={this.props.alc === 'alcohol'} />
-          <label htmlFor="alcohol" className="radio-label">Alcoholic</label>
-          <input type="radio" name="nonAlcohol" value="nonAlcohol" id="nonAlcohol"  onChange={this.filterAlcohol} checked={this.props.alc === 'nonAlcohol'}/>
-          <label htmlFor="nonAlcohol" className="radio-label">Non alcoholic</label>
+          <input
+            type="radio"
+            name="nonAlcohol"
+            value="all"
+            id="all"
+            onChange={this.filterAlcohol}
+            checked={this.props.alc === 'all'}
+          />
+          <label
+            htmlFor="all"
+            className="radio-label"
+          >
+            All types
+          </label>
+          <input
+            type="radio"
+            name="nonAlcohol"
+            value="alcohol"
+            id="alcohol"
+            onChange={this.filterAlcohol}
+            checked={this.props.alc === 'alcohol'}
+          />
+          <label
+            htmlFor="alcohol"
+            className="radio-label"
+          >
+            Alcoholic
+          </label>
+          <input
+            type="radio"
+            name="nonAlcohol"
+            value="nonAlcohol"
+            id="nonAlcohol"
+            onChange={this.filterAlcohol}
+            checked={this.props.alc === 'nonAlcohol'}
+          />
+          <label
+            htmlFor="nonAlcohol"
+            className="radio-label"
+          >
+            Non
+          </label>
         </div>
       </div>
     );
