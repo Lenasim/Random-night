dd4e9c823702ef4cd554378415843cff9b73963c
Lola <lola.donval@gmail.com>
2020-05-12 14:34:04 +0200

    css on lock unlock icon

--- src/components/ResultView/Card.js
+++ src/components/ResultView/Card.js
@@ -2,17 +2,15 @@ import React from 'react';
 
 const Card = props => {
   return (
-    <div>
-      <div className="center">
-        <div className="property-card">
-          <img className='property-image' src={props.image} alt={props.name} onClick={props.onClick} />
-          <div className="property-description">
-            <h4 className="card-category">{props.category}</h4>
-            <h3 className="card-title">{props.name}</h3>
-          </div>
-          <div className="property-social-icons">
-            <i onClick={props.isFav} className={props.class}></i>
-          </div>
+    <div className="center">
+      <div className="property-card">
+        <img className='property-image' src={props.image} alt={props.name} onClick={props.onClick} />
+        <div className="property-description">
+          <h4 className="card-category">{props.category}</h4>
+          <h3 className="card-title">{props.name}</h3>
+        </div>
+        <div className="property-social-icons">
+          <i onClick={props.isFav} className={props.class}></i>
         </div>
       </div>
     </div>
