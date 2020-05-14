06813aa88b323a29b0631cafdd7f0fa6f9d75161
Lola <lola.donval@gmail.com>
2020-05-14 11:42:26 +0200

    change place of button random when we click on filter button

--- src/App.js
+++ src/App.js
@@ -163,7 +163,7 @@ class App extends Component {
           />
           <div>
             {
-              !this.state.firstClick &&
+              !this.state.firstClick && !this.state.filterClick &&
               <div className="notice-button">
                 <Button
                   text={!this.state.firstClick ? "Get your plan" : "Try again?"}
@@ -179,37 +179,44 @@ class App extends Component {
                 }
               </div>
             }
-          </div>
-          {
-            this.state.filterClick &&
-            <div>
-              <div className="FilterDisplay">
-                <FilterButtons
-                  close={this.handleCloseFilters}
-                  filter={this.state.filterClick}
-                  reset={this.handleResetFilters}
-                  handleChange={this.handleChangeItem}
-                  activeId={this.state.activeId}
-                />
-                <div>{this.getItemContent()}</div>
+            {
+              this.state.filterClick &&
+              <div>
+                <div className="FilterDisplay">
+                  <FilterButtons
+                    close={this.handleCloseFilters}
+                    filter={this.state.filterClick}
+                    reset={this.handleResetFilters}
+                    handleChange={this.handleChangeItem}
+                    activeId={this.state.activeId}
+                  />
+                  <div>{this.getItemContent()}</div>
+                </div>
+                {
+                  !this.state.firstClick &&
+                  <Button
+                    text={!this.state.firstClick ? "Get your plan" : "Try again?"}
+                    isClicked={this.handleButtonClick}
+                  />
+                }
               </div>
-            </div>
-          }
-          {
-            this.state.firstClick &&
-            <Results
-              filter={this.state.filterClick}
-              filterClick={this.handleFilterClick}
-              drinkCategory={this.state.drinkCat}
-              drinkAlcohol={this.state.isAlcohol}
-              mealCat={this.state.mealCat}
-              mealIngr={this.state.mealIngr}
-              mealAreas={this.state.mealAreas}
-              movieGenre={this.state.genresResult}
-              cast={this.state.queryCast}
-              crew={this.state.queryCrew} />
-          }
-          <ScrollToTop />
+            }
+            {
+              this.state.firstClick &&
+              <Results
+                filter={this.state.filterClick}
+                filterClick={this.handleFilterClick}
+                drinkCategory={this.state.drinkCat}
+                drinkAlcohol={this.state.isAlcohol}
+                mealCat={this.state.mealCat}
+                mealIngr={this.state.mealIngr}
+                mealAreas={this.state.mealAreas}
+                movieGenre={this.state.genresResult}
+                cast={this.state.queryCast}
+                crew={this.state.queryCrew} />
+            }
+            <ScrollToTop />
+          </div>
         </div>
         <Footer />
       </div>
